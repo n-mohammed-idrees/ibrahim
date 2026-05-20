1625ff8a8712b1d72e8b59d0136534c32514e57a
n-mohammed-idrees <n.mohammedidrees19@gmail.com>
2026-05-20 14:12:09 +0530

    erro

--- app.py
+++ app.py
@@ -2,15 +2,18 @@ from flask import Flask,request,jsonify
 app=Flask(__name__)
 @app.route("/predict",methods=["POST"])
 def predict():
-    import numpy as np
-    import joblib
-    model=joblib.load("diabetes_model.pkl")
-    scaler=joblib.load("diabetes_scaler.pkl")
-    x=request.json
-    print(x)
-    x=np.asanyarray(x)
-    x=x.reshape(1,-1)
-    x=scaler.transform(x)
-    a=model.predict(x)
-    print(a)
-    return jsonify({"ppp":int(a)})
+    try:
+        import numpy as np
+        import joblib
+        model=joblib.load("diabetes_model.pkl")
+        scaler=joblib.load("diabetes_scaler.pkl")
+        x=request.json
+        print(x)
+        x=np.asanyarray(x)
+        x=x.reshape(1,-1)
+        x=scaler.transform(x)
+        a=model.predict(x)
+        print(a)
+        return jsonify({"ppp":int(a)})
+    except Exception as e:
+        return jsonify({"error",str(e)})
